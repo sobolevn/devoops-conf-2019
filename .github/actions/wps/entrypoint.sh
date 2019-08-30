88bb2d572a8b0ad6adc6c06432632f6645dedf9d
sobolevn <mail@sobolevn.me>
2019-08-30 15:29:03 +0300

    testing local action

---
+++ .github/actions/wps/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/env/bin bash
+set -e
+
+if [ "$#" -eq 0 ]; then
+  FLAKE8_COMMAND='flake8 .'
+else
+  FLAKE8_COMMAND="$*"
+fi
+
+echo "#########################################"
+echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+
+bash -c "$FLAKE8_COMMAND"
+
+echo "#########################################"
+echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
